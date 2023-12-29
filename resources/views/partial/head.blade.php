9d998632e4a7eab64f692d28ad173565794db606
KK IT DEVELOPER <itdeveloper2@kkad.com>
2023-12-29 16:05:50 +0530

    split layout

---
+++ resources/views/partial/head.blade.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="Saquib" content="Blade">
+<title>Amtex layout</title>
+<link rel="stylesheet" href="{{asset('vendors/perfect-scrollbar/perfect-scrollbar.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/bootstrap-icons/bootstrap-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="shortcut icon" href="{{asset('images/favicon.svg')}}" type="image/x-icon">
+@vite(['resources/js/app.js'])
